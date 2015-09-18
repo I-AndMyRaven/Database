3d9b54470096230255178426b8d7f6dcab1ade96
I-AndMyRaven <I.AndMyRaven@gmail.com>
2015-09-18 15:31:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,11 @@ function Init(){
   //Includes
   var cps = require('cps-api');
   //Creating a CPS connection
-  var cpsConn = new cps.Connection(  'tcp://cloud-eu-0.clusterpoint.com:9007', 'DB_NAME', 'USERNAME', 'PASSWORD', 'document', 'document/id', {account: YOUR_ACCOUNT_ID});
+  var cpsConn = new cps.Connection('tcp://cloud-eu-0.clusterpoint.com:9007', 'Test', 'prodevell', 'j6d8sd6g4', 'document', 'document/testid', {account: 101563});
   // Debug
   //cpsConn.debug = true;
 }
+
+Init();
+
+alert("Finish");
